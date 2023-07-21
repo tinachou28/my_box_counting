3d9d527f3eeb3330f640afabe19983a99b5756f5
brennansprinkle <brennan@u.northwestern.edu>
2023-07-20 18:21:03 -0600

    Update Fast_Box_Stats.py

--- Fast_Box_Stats.py
+++ Fast_Box_Stats.py
@@ -8,19 +8,19 @@ except ImportError:
 
 
 if __name__ == '__main__':
-    #########################################################
+    ################################################
     # set parameters for data
-    Lx = 320.0 # box size x-dir TODO: The C++ code is only setup to take Lp but it works with both
-    Ly = 320.0 # box size y-dir
-    Box_Ls = np.array([64.0, 32.0, 16.0, 8.0, 4.0, 2.0, 1.0, 0.5, 0.25, 0.125]) # array of box sizes to probe
-    infile = "./data/DfromPhi0.2_noHydro2D_nosteric_run_dt_0.003125_nsave_160.suspension_phi_0.34_L_320_modified.txt"
-    outfile = "./Count_Data_Cpp/No_Hydro_Py_Test_phi_0.34"
-    Nframes = 11830 # number of data frames
-    #infile = "./data/spec_softetakt_long_run_dtau_0.025_nsave_2.suspension_phi_0.34_L_320_modified.txt"
-    #outfile = "./Count_Data_Cpp/Py_Test_phi_0.34" #
-    #Nframes = 26478 # number of data frames
+    Lx = 217.6 # box size x-dir 
+    Ly = 174 # box size y-dir
+    Box_Ls = np.array([64.0, 32.0, 16.0, 8.0, 4.0, 2.0, 1.0, 0.5, 0.25]) # array of box sizes to probe
+    #modfile = "./data/noHydro2D_midpoint_run_dt_0.025_nsave_20.suspension_phi_0.34_L_320.config"
+    #Nframes = ConvertDataFile(modfile)
+    infile = "./data/c9allpositionssmall_realunits.dat"
+    outfile = "./Count_Data_Cpp/Exp_test" #"./Count_Data_Cpp/New_NH_Py_Test_long_phi_0.34"
+    Nframes = 2390 # number of data frames
     a = 1.395 #radius of particles
-    sep = np.array([2*a, 2*a, 2*a, 2*a, 2*a, 2*a, 2*a, 4*a, 4*a, 4*a]) #3*a #separation between boxes
+    sep = np.array([2*a, 2*a, 2*a, 2*a, 2*a, 2*a, 4*a, 4*a, 4*a]) #3*a #separation between boxes
+    #Calc_MSD_and_Output(infile, outfile, Nframes)
     Calc_and_Output_Stats(infile, outfile, Nframes, Lx, Ly, Box_Ls, sep)
     ###########################################################
     # For the cpp module
